747274babf47e3213f455cb0073d9924b3a24755
Shane Preece <shane@leafcanvas.com>
2022-04-07 19:16:01 +0100

    Apparently, you don't even need the starting pipe
    This changed everything.

--- lib/render-largest-contentful-paint.rb
+++ lib/render-largest-contentful-paint.rb
@@ -11,24 +11,24 @@ lcp = Dir.glob('lighthouse/*.json')
     { date: date, metric: metrics[:audits][:'largest-contentful-paint'] }
   end
 
-table_head = ""
-table_values = ""
-table_seperator = lcp.map { |_| '|---' }.join
+table_head = []
+table_values = []
+table_seperator = lcp.map { |_| '---' }.join('|')
 
 lcp.each_with_index do |metric, index|
   day_before = lcp[index + 1]
 
-  table_head += " | #{metric[:date]}"
+  table_head << metric[:date]
 
   label = ""
   label += "❌ " if day_before && metric[:metric][:numericValue] > day_before[:metric][:numericValue]
   label += "✅ " if day_before && metric[:metric][:numericValue] < day_before[:metric][:numericValue]
   label += metric[:metric][:displayValue]
 
-  table_values += " | #{label}"
+  table_values << label
 end
 
-for_render = "\n" + table_head + "\n " + table_seperator + "\n" + table_values + "\n"
+for_render = [ table_head.join(' | '), table_seperator, table_values.join(' | ') ].join("\n")
 
-readme = File.read('README.md').gsub(%r{<!-- lcp -->.*<!-- /lcp -->}m, "<!-- lcp -->#{for_render}<!-- /lcp -->")
+readme = File.read('README.md').gsub(%r{<!-- lcp -->.*<!-- /lcp -->}m, "<!-- lcp -->\n#{for_render}\n<!-- /lcp -->")
 File.write('README.md', readme)
